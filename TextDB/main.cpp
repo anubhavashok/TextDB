b303c85fa5b3db3762a582705f6aa91a770870e2
Anubhav Ashok <anubhav_ashok@apple.com>
2014-08-24 16:26:02 -0700

    Initial Commit

---
+++ TextDB/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TextDB
+//
+//  Created by Anubhav on 8/24/14.
+//  Copyright (c) 2014 anubhav. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
