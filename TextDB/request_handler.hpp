bc10b879fa74bab5d643d9c91b4e2b20f93e24f8
Anubhav Ashok <anubhavashok@utexas.edu>
2015-08-08 21:00:29 -0500

    Made constructor unexplicit

--- TextDB/request_handler.hpp
+++ TextDB/request_handler.hpp
@@ -29,14 +29,14 @@ public:
   request_handler& operator=(const request_handler&) = delete;
 
   /// Construct with a directory containing files to be served.
-  explicit request_handler(const std::string& doc_root);
+  request_handler(const std::string& doc_root);
 
   /// Handle a request and produce a reply.
   void handle_request(const request& req, reply& rep);
 
 private:
   /// The directory containing the files to be served.
- std::string doc_root_;
+  std::string doc_root_;
 
   /// Perform URL-decoding on a string. Returns false if the encoding was
   /// invalid.
